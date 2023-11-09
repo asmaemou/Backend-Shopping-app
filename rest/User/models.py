a27d8b0d489a457415c278076ac25b0e4ba938d1
asmaemou <amouradi2002@gmail.com>
2023-11-09 22:12:16 +0100

    updating the names

--- rest/User/models.py
+++ rest/User/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 class User(models.Model):
 
-    user_fname = models.CharField(max_length=20)
-    user_lname = models.CharField(max_length=20)
-    user_email = models.CharField(max_length=20)
-    user_password = models.CharField(max_length=20)
-    user_dob = models.CharField(max_length=20)
-    user_picture = models.CharField(max_length=20)
-    user_country = models.CharField(max_length=20)
-    user_status = models.CharField(max_length=20)
-    user_role = models.CharField(max_length=20)
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    dob = models.CharField(max_length=20)
+    picture = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
+    role = models.CharField(max_length=20)
 
 
     def __str__(self) :
--- rest/category/models.py
+++ rest/category/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     
-    category_Name = models.CharField(max_length=50)
-    category_Description=models.CharField(max_length=40)
+    name = models.CharField(max_length=50)
+    description=models.CharField(max_length=40)
 
     def __str__(self):
         return self.category_Name
