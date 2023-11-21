793ffecd59363947cee5d37dc1b8029d10c9add6
asmaemou <amouradi2002@gmail.com>
2023-11-21 10:16:25 +0100

    update the price
    price

--- rest/NewCollection/models.py
+++ rest/NewCollection/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class NewCollection(models.Model):
     picture = models.ImageField(upload_to='images/', null=True, blank=True)
-    amount = models.DecimalField(max_digits=19, decimal_places=10,default=0.00)
+    amount = models.DecimalField(max_digits=19, decimal_places=2,default=0.00)
 
 
