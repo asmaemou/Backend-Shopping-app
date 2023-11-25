abbae71b72a52ace39f0111cf87e83a18f00c100
asmaemou <amouradi2002@gmail.com>
2023-11-26 00:57:11 +0100

    add quantity

--- rest/Product/models.py
+++ rest/Product/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     amount = models.DecimalField(max_digits=19, decimal_places=2,default=0.00)
     status = models.CharField(max_length=20)
     manufacturer = models.CharField(max_length=20)
+    quantity = models.IntegerField(default=0)
     picture = models.ImageField(upload_to='images/', null=True, blank=True)
       
 
