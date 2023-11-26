30b6810c91809daca5dc409f9bf48754430563c5
asmaemou <amouradi2002@gmail.com>
2023-11-26 13:22:05 +0100

    new collection

--- rest/NewCollection/models.py
+++ rest/NewCollection/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 class NewCollection(models.Model):
-    picture = models.ImageField(upload_to='images/', null=True, blank=True)
-    amount = models.DecimalField(max_digits=19, decimal_places=2,default=0.00)
 
+    # picture = models.ImageField(upload_to='images/', null=True, blank=True)
+    # amount = models.DecimalField(max_digits=19, decimal_places=2,default=0.00)
+    # name = models.CharField(max_length=20)
+
+
+    description = models.CharField(max_length=20,default="no description")
+    stock = models.IntegerField(default=1)
+    rating = models.IntegerField(default=1)
+    name = models.CharField(max_length=20,default="no name")
+    amount = models.DecimalField(max_digits=19, decimal_places=2,default=0.00)
+    status = models.CharField(max_length=20,default="no satus")
+    manufacturer = models.CharField(max_length=20,default="no manufacturer")
+    quantity = models.IntegerField(default=1)
+    picture = models.ImageField(upload_to='images/', null=True, blank=True)
+      
+
+    def __str__(self) :
+        return self.description
 
